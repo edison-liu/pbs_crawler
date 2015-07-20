290714f2b188350591e3173e3ef7583f278ccd57
Edison Liu <eliu@localhost.localdomain>
2015-07-20 11:16:05 +0800

    DEV: Optimation with OO. 2015/07/20

--- pbs_crawler.py
+++ pbs_crawler.py
@@ -3,36 +3,29 @@ import sys, os, re
 from HTMLParser import HTMLParser
 import urllib, urllib2
 
-pbs_url = 'http://www.pbs.org/newshour/bb/great-day-beach-stranded-great-white-shark/'
 
-prefix = 'great-day-beach-stranded-great-white-shark'
+class PbsHTMLParser(HTMLParser):
+    def init(self, url, prefix):
+        self.start_transcript = 'TRANSCRIPT'
+        self.end_transcript = 'SHARE ON FACEBOOK'
+        self.prefix = prefix
+        self.in_file = self.prefix + '.html'
+        self.out_file = self.prefix + '.txt'
+        self.mp3_downloaded = self.prefix + '.mp3'
+        self.pbs_url = url
 
-start_transcript = 'TRANSCRIPT'
-end_transcript = 'SHARE ON FACEBOOK'
-
-in_file = prefix + '.html'
-out_file = prefix + '.txt'
-
-mp3_downloaded = prefix + '.mp3'
-
-class MyHTMLParser(HTMLParser):
-    #def __init__(self):
-    #    pass
-
-    def my_init(self):
         self.start = False
         self.end = False
         self.mp3 = []
 
     def handle_data(self, data):
-        if re.search(start_transcript, data):
+        if re.search(self.start_transcript, data):
             self.start = True
-        if re.search(end_transcript, data):
+        if re.search(self.end_transcript, data):
             self.end = True
 
         if self.start and not self.end:
-            out_f.write("%s\r\n" % data.strip())
-
+            self.out_f.write("%s\r\n" % data.strip())
 
     def handle_starttag(self, tag, attrs):
         if (tag != 'a'):
@@ -42,25 +35,36 @@ class MyHTMLParser(HTMLParser):
             if (k == 'href'):
                 if re.search('mp3', v):
                     self.mp3 = v;
-                    print self.mp3
 
     def down_mp3(self):
         if self.mp3:
-            urllib.urlretrieve(self.mp3, filename = mp3_downloaded)
+            urllib.urlretrieve(self.mp3, filename = self.mp3_downloaded)
+
+    def load(self):
+        urllib.urlretrieve(self.pbs_url, self.in_file)
+
+        self.in_f = open(self.in_file, 'r')
+        self.out_f = open(self.out_file, 'w+')
+
+        self.in_html = self.in_f.read()
+
+        self.feed(self.in_html)
+        self.down_mp3()
+
+        self.in_f.close()
+        self.out_f.close()
+      
+        
+class PbsCrawler():
+    def __init__(self):
+        self.pbs_url = 'http://www.pbs.org/newshour/bb/great-day-beach-stranded-great-white-shark/'
+        self.prefix = 'great-day-beach-stranded-great-white-shark'
+
+    def run(self):
+        self.parser = PbsHTMLParser()
+        self.parser.init(self.pbs_url, self.prefix)
+        self.parser.load()
 
 
-parser = MyHTMLParser()
-parser.my_init()
-
-urllib.urlretrieve(pbs_url, in_file)
-
-in_f = open(in_file, 'r')
-out_f = open(out_file, 'w+')
-
-in_html = in_f.read()
-
-parser.feed(in_html)
-parser.down_mp3()
-
-in_f.close()
-out_f.close()
+if __name__ == '__main__':
+    PbsCrawler().run()
